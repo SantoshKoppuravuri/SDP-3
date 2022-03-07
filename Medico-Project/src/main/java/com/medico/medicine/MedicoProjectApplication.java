3384ab9659d5c5fbcbbc97051b6cd02349d6ad66
SantoshKoppuravuri <73770265+SantoshKoppuravuri@users.noreply.github.com>
2022-03-07 18:41:16 +0530

    Add files via upload

---
+++ Medico-Project/src/main/java/com/medico/medicine/MedicoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.medico.medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicoProjectApplication.class, args);
+	}
+
+}
